a024671c66dc3aa3c1accf025bf8351b4daaede7
Rafal <rafal.ss@interia.pl>
2021-06-16 10:54:02 +0200

    Update

--- src/main/client/expenses/src/components/auth/LoginModule.tsx
+++ src/main/client/expenses/src/components/auth/LoginModule.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
 import { ILogin } from 'models';
 import { useDispatch } from 'react-redux';
 import { actions } from 'actions';
+import { Link } from 'react-router-dom';
 
 const loginSchema = Yup.object().shape({
   email: Yup.string().required(),
@@ -19,48 +20,62 @@ const defaultLoginValues: ILogin = {
 const LoginModuleForm = () => {
   const dispatch = useDispatch();
   return (
-    <Formik
-      initialValues={defaultLoginValues}
-      validationSchema={loginSchema}
-      onSubmit={(values, act) => {
-        dispatch(
-          actions.auth.login({
-            email: values.email,
-            password: values.password,
-          })
-        );
-        act.setSubmitting(false);
-      }}
-    >
-      {({ handleSubmit, handleChange, values }) => (
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              name="email"
-              value={values.email}
-              onChange={handleChange}
-            />
-          </Form.Group>
+    <div className="login">
+      <Formik
+        initialValues={defaultLoginValues}
+        validationSchema={loginSchema}
+        onSubmit={(values, act) => {
+          dispatch(
+            actions.auth.login({
+              email: values.email,
+              password: values.password,
+            })
+          );
+          act.setSubmitting(false);
+          act.resetForm();
+        }}
+      >
+        {({ handleSubmit, handleChange, values }) => (
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={values.password}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      )}
-    </Formik>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Row noGutters={true}>
+              <Col xs="6">
+                <Button variant="primary" type="submit" className="buttons__width">
+                  Login
+                </Button>
+              </Col>
+              <Col xs="6">
+                <Link to="/register">
+                  <Button variant="primary" type="submit" className="buttons__width">
+                    Register
+                  </Button>
+                </Link>
+              </Col>
+            </Row>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
--- src/main/client/expenses/src/components/auth/LogoutModule.tsx
+++ src/main/client/expenses/src/components/auth/LogoutModule.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { useDispatch } from 'react-redux';
 import { actions } from 'actions';
 import { JWT_KEY } from 'utils';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LogoutModule = React.memo(() => {
   const dispatch = useDispatch();
@@ -10,6 +12,16 @@ const LogoutModule = React.memo(() => {
     dispatch(actions.auth.logout());
   });
 
-  return <div>Logout</div>;
+  return (
+    <Container className="login">
+      <Row className="justify-content-md-center">
+        <Link to="/login">
+          <Button variant="primary" type="submit" className="buttons__width">
+            Login
+          </Button>
+        </Link>
+      </Row>
+    </Container>
+  );
 });
 export { LogoutModule };
